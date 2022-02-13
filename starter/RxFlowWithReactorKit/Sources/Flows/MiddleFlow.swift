5f727cc7c062a26cd21885719a132a01967c382f
myeongsoo <lyine0924@gmail.com>
2022-02-14 08:46:43 +0900

    feature: MiddleFlow의 코디네이팅 로직을 구현했어요.

--- starter/RxFlowWithReactorKit/Sources/Flows/MiddleFlow.swift
+++ starter/RxFlowWithReactorKit/Sources/Flows/MiddleFlow.swift
@@ -10,7 +10,7 @@ import RxFlow
 import RxRelay
 
 struct MiddleStepper: Stepper {
-	var steps: PublishRelay<Step>
+	var steps: PublishRelay<Step> = .init()
 	
 	var initialStep: Step {
 		return SampleStep.middleIsRequired
@@ -46,6 +46,58 @@ final class MiddleFlow: Flow {
 	// MARK: Navigate
 	
 	func navigate(to step: Step) -> FlowContributors {
+		guard let step = step.asSampleStep else { return .none }
+		
+		switch step {
+			case .middleIsRequired:
+				return coordinateToMiddle()
+				
+			case .middleIsRequiredAgain:
+				return coordinateToMiddleFirst()
+				
+			case .middleDetailIsRequired:
+				return presentMiddleDetail()
+				
+			case let .settingAndAlertIsRequired(msg):
+				let step = SampleStep.settingAndAlertIsRequired(message: msg)
+				let contributor: FlowContributor = .forwardToParentFlow(withStep: step)
+				return .one(flowContributor: contributor)
+				
+			case .dismiss:
+				return dismissVC()
+				
+			default:
+				return .none
+		}
+	}
+}
+
+// MARK: Method
+
+private extension MiddleFlow {
+	func coordinateToMiddle() -> FlowContributors {
+		let reactor = MiddleReactor(provider: provider)
+		let vc = MiddleViewController(with: reactor)
+		self.rootViewController.setViewControllers([vc], animated: true)
+		return .one(flowContributor: .contribute(withNextPresentable: vc,
+																						 withNextStepper: reactor))
+	}
+	
+	func coordinateToMiddleFirst() -> FlowContributors {
+		if (self.rootViewController.viewControllers.first as? MiddleViewController) != nil {
+			return .none
+		} else {
+			return coordinateToMiddle()
+		}
+	}
+	
+	// TODO: MiddleDetail 화면이 추가되면 구현할 예정이에요.
+	func presentMiddleDetail() -> FlowContributors {
+		return .none
+	}
+	
+	func dismissVC() -> FlowContributors {
+		self.rootViewController.visibleViewController?.dismiss(animated: true)
 		return .none
 	}
 }
