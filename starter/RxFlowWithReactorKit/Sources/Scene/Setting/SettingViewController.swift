c39b60d69b490280a0f168d81dc1d01b8677723c
myeongsoo <lyine0924@gmail.com>
2022-02-14 11:25:34 +0900

    feautre: SettingView와 관련된 보일러플레이트 코드를 추가했어요.

---
+++ starter/RxFlowWithReactorKit/Sources/Scene/Setting/SettingViewController.swift
@@ -0,0 +1,65 @@
+//
+//  SettingViewController.swift
+//  RxFlowWithReactorKit
+//
+//  Created by Myeongsoo Park on 2022/02/14.
+//
+
+import UIKit
+import ReactorKit
+import RxCocoa
+
+final class SettingViewController: UIViewController {
+	var disposeBag: DisposeBag = .init()
+	
+	typealias Reactor = SettingReactor
+	
+	init(with reactor: Reactor) {
+		super.init(nibName: nil, bundle: nil)
+		
+		self.reactor = reactor
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	deinit {
+		print("\(type(of: self)): \(#function)")
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		setUI()
+	}
+}
+
+// MARK: - Extensions
+
+private extension SettingViewController {
+	func setUI() {
+		self.title = "Middle"
+		view.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+	}
+}
+
+extension SettingViewController: View {
+	func bind(reactor: Reactor) {
+		bindView(reactor)
+		bindAction(reactor)
+		bindState(reactor)
+	}
+	
+	private func bindView(_ reactor: Reactor) {
+		
+	}
+	
+	private func bindAction(_ reactor: Reactor) {
+		
+	}
+	
+	private func bindState(_ reactor: Reactor) {
+		
+	}
+}
