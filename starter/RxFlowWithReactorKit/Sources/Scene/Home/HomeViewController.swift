2c07fc1c6ff77bf7166c81ffd659b602010f75f3
myeongsoo <lyine0924@gmail.com>
2022-02-15 10:10:49 +0900

    feature: 홈 화면에 TableView와 관련된 UI 코드를 작성했어요.

--- starter/RxFlowWithReactorKit/Sources/Scene/Home/HomeViewController.swift
+++ starter/RxFlowWithReactorKit/Sources/Scene/Home/HomeViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 import RxFlow
 import RxCocoa
 import ReactorKit
+import Then
 
 final class HomeViewController: UIViewController, View {
 	
 	typealias Reactor = HomeReactor
 	
 	var disposeBag: DisposeBag = .init()
+	
+	private let tableView = UITableView().then {
+		$0.register(HomeCell.self, forCellReuseIdentifier: HomeCell.reusableID)
+		$0.rowHeight = 50
+		$0.separatorStyle = .none
+	}
 	
 	init(with reactor: HomeReactor) {
 		super.init(nibName: nil, bundle: nil)
@@ -42,6 +49,11 @@ private extension HomeViewController {
 	func setUI() {
 		self.title = "Home"
 		view.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
+		
+		view.addSubview(tableView)
+		tableView.snp.makeConstraints {
+			$0.edges.equalTo(view.safeArea.edges)
+		}
 	}
 }
 
