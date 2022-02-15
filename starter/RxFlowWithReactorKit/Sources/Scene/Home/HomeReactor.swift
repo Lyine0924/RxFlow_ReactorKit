55b2f44e3a73bcd183c900cc504760162dcc1d47
myeongsoo <lyine0924@gmail.com>
2022-02-15 11:25:02 +0900

    feature: HomeReactor의 Mutation과 State 로직을 구현했어요.

--- starter/RxFlowWithReactorKit/Sources/Scene/Home/HomeReactor.swift
+++ starter/RxFlowWithReactorKit/Sources/Scene/Home/HomeReactor.swift
@@ -24,11 +24,14 @@ final class HomeReactor: Reactor, Stepper {
 	}
 	
 	enum Mutation {
-
+		case setMeta(_ meta: MetaData)
+		case setMovies(_ movies: [Movie])
+		case setError(error: Error)
 	}
 	
 	struct State {
-
+		var meta: MetaData?
+		var movies: [Movie]?
 	}
 	
 	// MARK: Properties
@@ -44,6 +47,8 @@ final class HomeReactor: Reactor, Stepper {
 	}
 }
 
+// MARK: - Extensions
+
 // MARK: Mutation
 
 extension HomeReactor {
@@ -57,7 +62,31 @@ extension HomeReactor {
 extension HomeReactor {
 	func reduce(state: State, mutation: Mutation) -> State {
 		var newState = state
+		
+		switch mutation {
+			case .setMeta(let meta):
+				newState.meta = meta
+			case .setMovies(let movies):
+				newState.movies = movies
+			case .setError(let error):
+				errorSubject.onNext(error)
+		}
+		
 		return newState
 	}
 }
 
+// MARK: Method
+
+private extension HomeReactor {
+	func fetchMovieResult() -> Observable<Mutation> {
+		let fetchResult = provider.networkService.fetchMovies()
+		
+		switch fetchResult {
+			case .success(let result):
+				return .of(.setMeta(result.metaData), .setMovies(result.movie))
+			case .failure(let error):
+				return .just(.setError(error: error))
+		}
+	}
+}
