f19dc32d4ce9319a96fdb9fcf4d00b57cfe6535c
myeongsoo <lyine0924@gmail.com>
2022-02-15 10:07:59 +0900

    feature: HomeCell의 UI코드를 구현했어요.

--- starter/RxFlowWithReactorKit/Sources/Scene/Home/HomeCell.swift
+++ starter/RxFlowWithReactorKit/Sources/Scene/Home/HomeCell.swift
@@ -11,9 +11,16 @@ import ReactorKit
 
 final class HomeCell: UITableViewCell {
 	
+	let titleLabel: UILabel = UILabel().then {
+		$0.font = .systemFont(ofSize: 12)
+		$0.textAlignment = .center
+	}
+	
 	// MARK: Initializers
 	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
 		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		
+		self.setUI()
 	}
 	
 	required init?(coder: NSCoder) {
@@ -22,5 +29,22 @@ final class HomeCell: UITableViewCell {
 	
 	override func prepareForReuse() {
 		
+	}
+}
+
+// MARK: - Extensions
+
+extension HomeCell {
+	func bind(with item: Movie) {
+		titleLabel.text = item.description
+	}
+	
+	private func setUI() {
+		contentView.backgroundColor = .white
+		
+		contentView.addSubview(titleLabel)
+		titleLabel.snp.makeConstraints {
+			$0.edges.equalToSuperview()
+		}
 	}
 }
