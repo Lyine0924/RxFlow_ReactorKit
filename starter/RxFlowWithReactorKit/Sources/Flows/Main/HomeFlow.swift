e4c38b4c30dc48519ab5745a6bdd10210bf4dedf
myeongsoo <lyine0924@gmail.com>
2022-02-27 16:17:31 +0900

    feature: HomeFlow에 detail로 이동하기 위한 step case와 메소드를 구현했어요.

--- starter/RxFlowWithReactorKit/Sources/Flows/Main/HomeFlow.swift
+++ starter/RxFlowWithReactorKit/Sources/Flows/Main/HomeFlow.swift
@@ -52,6 +52,8 @@ final class HomeFlow: Flow {
 		switch step {
 			case .homeIsRequired:
 				return coordinateToHome()
+			case .homeItemIsPicked(let movieID):
+				return coordinateToHomeDetail(with: movieID)
 			default:
 				return .none
 		}
@@ -68,5 +70,13 @@ private extension HomeFlow {
 		self.rootViewController.setViewControllers([viewController], animated: true)
 		
 		return .one(flowContributor: .contribute(withNextPresentable: viewController, withNextStepper: reactor))
+	}
+	
+	func coordinateToHomeDetail(with ID: String) -> FlowContributors {
+		let reactor = HomeDetailReactor(provider: provider)
+		let vc = HomeDetatilViewController(with: reactor, title: ID)
+		self.rootViewController.pushViewController(vc, animated: true)
+		return .one(flowContributor: .contribute(withNextPresentable: vc,
+																						 withNextStepper: reactor))
 	}
 }
