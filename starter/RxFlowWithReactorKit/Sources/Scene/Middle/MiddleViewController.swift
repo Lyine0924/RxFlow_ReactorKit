f019d80e8ebb5f152e552f5479d47315ae3416a3
myeongsoo <lyine0924@gmail.com>
2022-02-27 15:17:37 +0900

    feature: Middle화면의 UI를 구현했어요.

--- starter/RxFlowWithReactorKit/Sources/Scene/Middle/MiddleViewController.swift
+++ starter/RxFlowWithReactorKit/Sources/Scene/Middle/MiddleViewController.swift
@@ -14,6 +14,23 @@ final class MiddleViewController: UIViewController {
 	
 	typealias Reactor = MiddleReactor
 	
+	// MARK: UI Properties
+	
+	private let detailButton: UIButton = UIButton().then {
+		$0.setTitle("toMiddleDetail", for: .normal)
+		$0.backgroundColor = .black
+	}
+	
+	private let twoDepthFlow: UIButton = UIButton().then {
+		$0.setTitle("twoDepthFlow", for: UIControl.State())
+		$0.backgroundColor = .black
+	}
+	
+	private let transitionButton: UIButton = UIButton().then {
+		$0.setTitle("transitionButton", for: UIControl.State())
+		$0.backgroundColor = .black
+	}
+	
 	init(with reactor: Reactor) {
 		super.init(nibName: nil, bundle: nil)
 		
@@ -41,6 +58,27 @@ private extension MiddleViewController {
 	func setUI() {
 		self.title = "Middle"
 		view.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+		
+		view.addSubview(detailButton)
+		detailButton.snp.makeConstraints {
+			$0.leading.trailing.equalToSuperview().inset(50)
+			$0.bottom.equalTo(view.safeArea.bottom).inset(50)
+			$0.height.equalTo(50)
+		}
+		
+		view.addSubview(twoDepthFlow)
+		twoDepthFlow.snp.makeConstraints {
+			$0.leading.trailing.equalToSuperview().inset(50)
+			$0.bottom.equalTo(detailButton.snp.top).offset(-50)
+			$0.height.equalTo(50)
+		}
+		
+		view.addSubview(transitionButton)
+		transitionButton.snp.makeConstraints {
+			$0.leading.trailing.equalToSuperview().inset(50)
+			$0.bottom.equalTo(twoDepthFlow.snp.top).offset(-50)
+			$0.height.equalTo(50)
+		}
 	}
 }
 
